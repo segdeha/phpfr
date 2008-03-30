6687c0d794cf8589c77e0b686aca9217bf592cf4
segdeha <segdeha@b981e50e-7c23-0410-81f4-871046dce75c>
2008-03-30 09:33:48 +0000

    Added Romanian localization (thanks, Seb). Updated help, though it's not quite finished.

---
+++ PHPfr.wdgt/ro.lproj/LocalizedStrings.js
@@ -0,0 +1,36 @@
+// Romanian version by Sergiu Sebastian Tauciuc, http://www.sergiutauciuc.ro/en/
+
+var LocalizedStrings = new Object;
+
+//_______________ENGLISH_PHRASE_______________LOCALIZED_VERSION_____________________________
+LocalizedStrings['Tip:']                   = 'Sfat:';
+LocalizedStrings['Done']                   = 'Gata';
+LocalizedStrings['Donate']                 = 'Donează';
+LocalizedStrings['--Select a Topic--']     = '--Selectează un subiect--';
+LocalizedStrings['--Select a Size--']      = '--Selectează o dimensiune--';
+LocalizedStrings['Show Favorites']         = 'Arată favorite';
+LocalizedStrings['Show Cheat Sheet']       = 'Arată sfaturi utile';
+LocalizedStrings['Show Date Formatter']    = 'Arată opţiuni de formatare a datei'; 
+LocalizedStrings['Show PHPfr Help']        = 'Arată ajutor PHPfr';
+LocalizedStrings['Close Page Drawer']      = 'Închide sertar pagină';
+LocalizedStrings['Open Page Drawer']       = 'Deschide sertar pagină';
+LocalizedStrings['Loading']                = 'Se încarcă';
+LocalizedStrings['Check for new version.'] = 'Caută o nouă versiune.';
+LocalizedStrings['Checking&#8230;']        = 'Se caută&#8230;';
+LocalizedStrings['Click to download new version!'] = 'Click pentru a descărca noua versiune!';
+LocalizedStrings['This widget is up-to-date.']     = 'Acest widget este la zi.';
+LocalizedStrings['There was a problem checking the version.'] = 'A intervenit o problemă la aflarea versiunii.';
+LocalizedStrings['Click to add this page to your favorites']             = 'Click pentru a adăuga pagina la favorite';
+LocalizedStrings['Click to remove this page from your favorites']        = 'Click pentru a scoate pagina de la favorite';
+LocalizedStrings['Double-click a function name to see it on PHP.net']    = 'Dublu-click pe numele unei funcţii pentru a o vedea pe PHP.net';
+LocalizedStrings['Use regular expressions to search, e.g., ^.*[0-9]$']   = 'Foloseşte expresii regulate pentru căutare, e.g., ^.*[0-9]$';
+LocalizedStrings['Copy selected text in the function frame using cmd-c'] = 'Copie textul selectat în numele funcţiei folosind cmd-c';
+LocalizedStrings['Scroll the function list using up- and down-arrows']   = 'Navighează în lista de funcţii folosind săgeţile sus şi jos';
+LocalizedStrings['Add functions to your favorites using the heart icon'] = 'Adaugă funcţii la favorite folosind iconiţa cu inima';
+LocalizedStrings['Open the drawer to resize the view frame']             = 'Deschide sertarul pentru a redimensiona cadrul de vizualizare';
+LocalizedStrings['Follow links on function pages to view more PHP docs'] = 'Urmează link-urile de pe paginile funcţiilor pentru a vedea mai multe documente PHP.';
+LocalizedStrings['Click in the empty search box and hit return to see all functions'] = 'Click în căsuţa de căutare goală şi enter pentru a vedea toate funcţiile';
+LocalizedStrings['Make a developer happy by donating a few dollars :-)'] = 'Fă un programator fericit cu o mică donaţie :-)';
+LocalizedStrings['Click the language name to install/update. Click the radio button to select your language.'] = 'Click pe numele limbii pentru a instala/actualiza. Click pe butonul radio pentru a selecta limba.';
+LocalizedStrings['Elapsed time:'] = 'Elapsed time:';
+LocalizedStrings['seconds']       = 'seconds';
