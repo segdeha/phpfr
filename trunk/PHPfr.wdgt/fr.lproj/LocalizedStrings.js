535363be9ee0136e39d7939332c82d92339059ca
segdeha <segdeha@b981e50e-7c23-0410-81f4-871046dce75c>
2008-04-03 23:34:40 +0000

    Added French localization

---
+++ trunk/PHPfr.wdgt/fr.lproj/LocalizedStrings.js
@@ -0,0 +1,37 @@
+// French version by Barbara Napholtz
+
+var LocalizedStrings = new Object;
+
+//_______________ENGLISH_PHRASE_______________LOCALIZED_VERSION_____________________________
+LocalizedStrings['Tip:']                   = 'Conseils:';
+LocalizedStrings['Done']                   = 'Finé';
+LocalizedStrings['Donate']                 = 'Faire un don';
+LocalizedStrings['--Select a Topic--']     = '--Sélectionnez une Rubrique--';
+LocalizedStrings['--Select a Size--']      = '--Sélectionnez une Taille--';
+LocalizedStrings['Show Favorites']         = 'Afficher Favoris';
+LocalizedStrings['Show Cheat Sheet']       = 'Afficher la feuille de triche';
+LocalizedStrings['Show Date Formatter']    = 'Afficher la formatter de la date';
+LocalizedStrings['Show PHPfr Help']        = 'Afficher PHPfr Aide';
+LocalizedStrings['Close Page Drawer']      = 'Fermer la page tiroir';
+LocalizedStrings['Open Page Drawer']       = 'Ouvrez la page de tiroir';
+LocalizedStrings['Loading']                = 'Chargement';
+LocalizedStrings['Check for new version.'] = 'Vérifier la nouvelle version.';
+LocalizedStrings['Checking&#8230;']        = 'Vérification&#8230;';
+LocalizedStrings['Click to download new version!'] = '
+Cliquer ici pour télécharger la nouvelle version!';
+LocalizedStrings['This widget is up-to-date.']     = 'Ce widget est à jour.';
+LocalizedStrings['There was a problem checking the version.'] = 'Il y avait un problème de la vérification de la version.';
+LocalizedStrings['Click to add this page to your favorites']             = 'Cliquez ici pour ajouter cette page à vos favoris';
+LocalizedStrings['Click to remove this page from your favorites']        = 'Cliquer ici pour supprimer cette page de vos favoris';
+LocalizedStrings['Double-click a function name to see it on PHP.net']    = 'Double-cliquez sur un nom de fonction pour les voir sur PHP.net';
+LocalizedStrings['Use regular expressions to search, e.g., ^.*[0-9]$']   = 'Utiliser des expressions régulières pour la recherche, e.g., ^.*[0-9]$';
+LocalizedStrings['Copy selected text in the function frame using cmd-c'] = 'Copiez le texte sélectionné dans le cadre en utilisant cmd-c';
+LocalizedStrings['Scroll the function list using up- and down-arrows']   = 'Faites défiler la liste en utilisant les flèches haut et bas';
+LocalizedStrings['Add functions to your favorites using the heart icon'] = 'Ajouter des fonctions à vos favoris en utilisant l\'icône de cœur';
+LocalizedStrings['Open the drawer to resize the view frame']             = 'Ouvrez le tiroir pour redimensionner l\'image vue';
+LocalizedStrings['Follow links on function pages to view more PHP docs'] = 'Suivez les liens sur les pages de fonctions pour voir PHP docs';
+LocalizedStrings['Click in the empty search box and hit return to see all functions'] = 'Cliquez dans le champ de recherche vide et appuyez retour pour voir toutes les fonctions';
+LocalizedStrings['Make a developer happy by donating a few dollars <span style="font-family: Verdana, Sans-Serif;">:-)</span>'] = 'Donnez une poignée de dollars et rendre un développeur heureux <span style="font-family: Verdana, Sans-Serif;">:-)</span>';
+LocalizedStrings['Click the language name to install/update. Click the radio button to select your language.']                  = 'Cliquez sur le nom de la langue pour l\'installation / actualiser. Cliquez sur le bouton radio pour choisir votre langue.';
+LocalizedStrings['Type here to filter list'] = 'Écrivez ici pour filtrer la liste';
+// LocalizedStrings[''] = '';
